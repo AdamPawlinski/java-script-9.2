acdac646196c2c727cfd8b1539fecafb9ed4c8ae
Adam Pawliński <adam.pawlinski@gmail.com>
2018-03-09 22:08:37 +0100

    remove alerts

--- js/script.js
+++ js/script.js
@@ -2,13 +2,10 @@ var maleName = ['Adam', 'Marek', 'Jan', 'Zdzisław', 'Kazimierz'];
 var femaleName = ['Karolina', 'Anna', 'Maria', 'Katarzyna', 'Małgorzata'];
 var allNames = maleName.concat(femaleName);
 console.log(allNames);
-alert(allNames);
 var newName = 'Marian';
 if (allNames.indexOf(newName) === -1) {
   allNames.push(newName);
 } else {
   console.log("empty array");
-  alert("empty array");
 }
 console.log(allNames);
-alert(allNames);
